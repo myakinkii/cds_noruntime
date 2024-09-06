0ea7166f42c63da044662f74ec7c1cc22980187f
alex <alexey.myakinky@dev4hana.ru>
2024-09-06 16:17:17 +0200

    minor change

--- index.js
+++ index.js
@@ -5,22 +5,24 @@ const port = process.env.PORT || 4004
 
 cds.connect("db").then(async function(db){
 
-    // some stuff from your custom server.js
     cds.on('bootstrap', (app)=>{
         // not gonna happen
     })
-    
+
+    if (cds.env.requires.messaging)
+        await cds.connect.to('messaging') // to init messaging
+
     cds.on('serving', (service) => {
-        const { Books } = service.entities
+        const { Books, Authors } = service.entities
     })
+
+    await cds.serve("all").in(app) // to serve all services
 
     cds.on('served', (services)=>{
-        const { CatalogService, db } = services
+        const { CatalogService, AdminService, db } = services
     })
 
-    // await cds.connect.to('messaging') // to init messaging
-    await cds.serve("all").in(app) // to serve all services
-    await cds.emit ('served', cds.services) // amag_common_messaging will register emit handlers now
+    await cds.emit ('served', cds.services) // all services served now
 
     app.use(express.static(__dirname+'/../../app')) // to serve static app 
     app.get('/', (req, res) => res.send("ok\n"))
