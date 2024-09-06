b0d7c74aeacfd9adbd401c4160868dc18f3f677d
alex <alexey.myakinky@dev4hana.ru>
2024-09-07 01:25:47 +0200

    move cds_serve out of main closure

--- index.js
+++ index.js
@@ -57,6 +57,59 @@ class ODataAdapter extends BaseProtocolAdapter {
   }
 }
 
+function cds_serve (som, _options){
+        
+    const services = som == "all" ? cds.model.services : [cds.model.services[som]]
+    let implClassess = services.reduce( (prev,cur) => Object.assign(prev, { [cur.name]: null }), {})
+
+    const o = {..._options} // this later is modified by our from/with/at/to
+
+    return {
+        from (model) {
+            o.from = model;
+            return this
+        },
+        with (impl) {
+            if (Array.isArray(impl)) {
+                impl.forEach( i => implClassess[i.name] = i)
+            } else { // not to break .with api when it is only a single impl
+                o.with = impl;
+                implClassess[impl.name] = impl
+            }
+            return this
+        },
+        at (path) {
+            o.at   = path;
+            return this
+        },
+        to (protocol) {
+            o.to   = protocol;
+            return this
+        },
+        async in (app) {
+
+            const instances = await Promise.all (services.map( async d => {
+                // looks like srv.endpoints are somehow async so we need to await new impl.. weird
+                const srv = new implClassess[d.name](d.name, cds.model, o)
+                return srv._init()
+            }))
+
+            // for now we will only focus on default v4 protocol
+            const { before, after } = cds.middlewares
+
+            instances.forEach (srv => {
+                for (let { kind, path } of srv.endpoints) {
+                    const adapter = new ODataAdapter(srv)
+                    adapter.path = path
+                    app.use(path, before, adapter, after) // ok there it is! our magic
+                    cds.emit ('serving', srv)
+                }
+            })
+            return this
+        }
+    }
+}
+
 cds.connect("db").then(async function(somehowThisIsCdsNow){
 
     // const csn = await cds.load('*').then(cds.minify) // cuz why not to load it again
@@ -81,59 +134,6 @@ cds.connect("db").then(async function(somehowThisIsCdsNow){
     // await cds_serve("AdminService").with(adminImpl).in(app)
     // await cds_serve("CatalogService").with(catalogImpl).at("/some-endpoint").to("odata-v4").in(app)
     await cds_serve("all").with([adminImpl, catalogImpl]).in(app) // to serve all services with impl classess
-
-    function cds_serve (som, _options){
-        
-        const services = som == "all" ? cds.model.services : [cds.model.services[som]]
-        let implClassess = services.reduce( (prev,cur) => Object.assign(prev, { [cur.name]: null }), {})
-
-        const o = {..._options} // this later is modified by our from/with/at/to
-
-        return {
-            from (model) {
-                o.from = model;
-                return this
-            },
-            with (impl) {
-                if (Array.isArray(impl)) {
-                    impl.forEach( i => implClassess[i.name] = i)
-                } else { // not to break .with api when it is only a single impl
-                    o.with = impl;
-                    implClassess[impl.name] = impl
-                }
-                return this
-            },
-            at (path) {
-                o.at   = path;
-                return this
-            },
-            to (protocol) {
-                o.to   = protocol;
-                return this
-            },
-            async in (app) {
-
-                const instances = await Promise.all (services.map( async d => {
-                    // looks like srv.endpoints are somehow async so we need to await new impl.. weird
-                    const srv = new implClassess[d.name](d.name, cds.model, o)
-                    return srv._init()
-                }))
-
-                // for now we will only focus on default v4 protocol
-                const { before, after } = cds.middlewares
-
-                instances.forEach (srv => {
-                    for (let { kind, path } of srv.endpoints) {
-                        const adapter = new ODataAdapter(srv)
-                        adapter.path = path
-                        app.use(path, before, adapter, after) // ok there it is! our magic
-                        cds.emit ('serving', srv)
-                    }
-                })
-                return this
-            }
-        }
-    }
 
     cds.on('served', (services)=>{
         const { CatalogService, AdminService, db } = services
