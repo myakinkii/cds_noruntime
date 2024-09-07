b08d722974270caf0d0dea799cb8456745776bfa
alex <alexey.myakinky@dev4hana.ru>
2024-09-07 04:45:34 +0200

    added srv methods to log and observe

--- srv/admin-service.js
+++ srv/admin-service.js
@@ -1,12 +1,33 @@
 const cds = require('@sap/cds/lib')
 
-module.exports = class AdminService extends cds.ApplicationService { init(){
-  this.before ('NEW','Books.drafts', genid)
-  return super.init()
-}}
+module.exports = class AdminService extends cds.ApplicationService { 
+  
+  init(){
+    const { Books } = this.entities
 
-/** Generate primary keys for target entity in request */
-async function genid (req) {
-  const {ID} = await cds.tx(req).run (SELECT.one.from(req.target.actives).columns('max(ID) as ID'))
-  req.data.ID = ID - ID % 100 + 100 + 1
+    this.before ('NEW', Books.drafts, this.genid)
+    return super.init()
+  }
+
+  async run(req, data){
+    console.log("RUN", typeof req)
+    return super.run(req, data)
+  }
+
+  async dispatch(req){
+    console.log("DISPATCH", req.event, JSON.stringify(req.query))
+    return super.dispatch(req)
+  }
+
+  async handle(req){
+    console.log("HANDLE", req.event, JSON.stringify(req.query))
+    return super.handle(req)
+  }
+
+  // Generate primary keys for target entity in request
+  async genid (req) {
+    const {ID} = await cds.tx(req).run (SELECT.one.from(req.target.actives).columns('max(ID) as ID'))
+    req.data.ID = ID - ID % 100 + 100 + 1
+  }
+  
 }
--- srv/cat-service.js
+++ srv/cat-service.js
@@ -3,13 +3,27 @@ const cds = require('@sap/cds')
 module.exports = class CatalogService extends cds.ApplicationService { 
   
   init(){
-    
     const { Books } = this.entities
 
     this.on ('submitOrder', this.handleSubmitOrder)
     this.after ('READ', Books, this.afterRead)
 
     return super.init()
+  }
+
+  async run(req, data){
+    console.log("RUN", typeof req)
+    return super.run(req, data)
+  }
+
+  async dispatch(req){
+    console.log("DISPATCH", req.event, JSON.stringify(req.query))
+    return super.dispatch(req)
+  }
+
+  async handle(req){
+    console.log("HANDLE", req.event, JSON.stringify(req.query))
+    return super.handle(req)
   }
 
   // Reduce stock of ordered books if available stock suffices
