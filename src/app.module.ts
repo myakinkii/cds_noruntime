3dfd6b908aab61e076f8ef144519a62d60442d21
alex <alexey.myakinky@dev4hana.ru>
2024-09-08 20:01:30 +0200

    simple controller and middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, RequestMethod, MiddlewareConsumer } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res, HttpStatus } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
 
-@Module({})
-export class AppModule {}
+@Controller('rest/v1/srv/Books')
+export class BooksController {
+    @Get()
+    get(@Res() res: Response) {
+        res.status(HttpStatus.OK).json([]);
+    }
+    @Post()
+    create(@Req() req: Request, @Res() res: Response) {
+        res.status(HttpStatus.CREATED).send(req.body); // jsonBodyParser from express.json()
+    }
+}
+
+function rootResponder(req: Request, res: Response, next: NextFunction) {
+    res.status(HttpStatus.OK).send("ok");
+};
+
+@Module({
+    controllers: [BooksController]
+})
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(rootResponder).forRoutes({ path: '/', method: RequestMethod.GET });
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express'
+import { Request, Response, NextFunction } from 'express';
 import { AppModule } from './app.module';
+
+function logger(req: Request, res: Response, next: NextFunction) {
+    console.log(`req ${req.url}`);
+    next();
+};
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(logger)
   await app.listen(3000);
 }
 bootstrap();
